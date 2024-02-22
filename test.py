8ed2e6840b8d4227bbc519696b9efb6783f4754a
LeangSun <123008712+LeangSun@users.noreply.github.com>
2024-02-22 21:04:14 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
-from functions import get_match, get_parsed_match, test_players, get_player_match
-result = test_players(876279809, 1698864000)
-print(result)
+from functions import get_match, get_parsed_match, test_players, get_player_match, get_player_list
+import json
+
+filepath = r"C:\Users\Sunle\PycharmProjects\pythonProject\OpenDota\Main\account_id.json"
+player_list = get_player_list(6, filepath, 7601780874)
