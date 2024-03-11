18c13601fbb7e5e0eaa0eb8aba676f2f46315b7f
LeangSun <123008712+LeangSun@users.noreply.github.com>
2024-03-11 22:49:41 +0000

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-from functions import get_match, get_parsed_match, test_players, get_player_match, get_player_list
-import json
-
-filepath = r"C:\Users\Sunle\PycharmProjects\pythonProject\OpenDota\Main\account_id.json"
-player_list = get_player_list(6, filepath, 7601780874)
