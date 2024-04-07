72f0501512ca301da96f12af7ba1b2efc7c8469d
LeangSun <123008712+LeangSun@users.noreply.github.com>
2024-04-07 05:53:56 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,8 +1,9 @@
-import requests
-import time
-import os
 import json
+import os
 import sys
+import time
+
+import requests
 
 
 def read_json(filepath):
@@ -37,8 +38,23 @@ def get_match(match_id):
         print(response.text)
 
 
-def get_player_match(account_id):
+def get_player_match(account_id, project=None):
     url = f"https://api.opendota.com/api/players/{account_id}/matches"
+    query_params = {
+        "project": str(project)
+    }
+    response = requests.get(url, params=query_params)
+
+    if response.status_code == 200:
+        account_data = response.json()
+        return account_data
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.text)
+
+
+def get_player_recent_match(account_id):
+    url = f"https://api.opendota.com/api/players/{account_id}/recentMatches"
 
     response = requests.get(url)
 
@@ -58,7 +74,31 @@ def get_player(account_id):
         return account_data
     else:
         print(f"Error: {response.status_code}")
-        print(response.text)  
+        print(response.text)
+
+
+def get_public_match(number_in_hundreds, less_than_match_id=None):
+    url = f"https://api.opendota.com/api/publicMatches"
+    public_match_number = range(number_in_hundreds)
+
+    full_list_public_match = []
+    for i in public_match_number:
+        query_params = {
+            "less_than_match_id": less_than_match_id
+        }
+        response = requests.get(url, params=query_params)
+        if response.status_code == 200:
+            public_match = response.json()
+
+            full_list_public_match.extend(public_match)
+            less_than_match_id = int(public_match[-1]["match_id"])
+            time.sleep(1.5)
+            print("Succeed in getting 100 public match")
+
+        else:
+            print(f"Error: {response.status_code}")
+            print(response.text)
+    return full_list_public_match
 
 
 def get_parsed_match(number_in_hundreds, less_than_match_id=None):
@@ -92,11 +132,10 @@ def get_parsed_match(number_in_hundreds, less_than_match_id=None):
 
 # This function provides a list of parsed matches just ended, which can be considered as random sampling for matches.
 
-def get_player_list(hundreds_of_parsed_match, filepath, less_than_match_id=None):
-    parsed_match_index = get_parsed_match(hundreds_of_parsed_match, less_than_match_id)
+def get_player_list(match_index, filepath, less_than_match_id=None):
     account_ids = []
     i = 0
-    for index in parsed_match_index:
+    for index in match_index:
         match = None
         while match is None:
             match = get_match(index)
@@ -124,10 +163,14 @@ def retrieve_player_info(account_id):
     player_info.append(account_info)
 
     total_match_info = get_player_match(account_id)
+    time.sleep(1.5)
     player_info.append(total_match_info)
 
+    recent_match_info = get_player_recent_match(account_id)
     time.sleep(1.5)
-    if len(player_info) == 2:
+    player_info.append(recent_match_info)
+
+    if len(player_info) == 3:
         print(f"Succeed in retrieving {account_id}")
     else:
         print("Failure! The last account_id tried is {account_id}")
@@ -162,6 +205,47 @@ def test_players(info, timestamp):
 
     return player_info
 
+
 # This function provides information about the quality of a player's data.
 # Information: Rank tier; Total number of matches; Number of matches before the timestamp;
 # Number of parsed matches before the timestamp; Percentage of parsed matches before the time stamp
+
+def search_id(time_target, hundreds_of_match=1):  # 这个time应该是某日凌晨一点的timestamp
+    id_1 = 7288601836
+    id_2 = id_1 + (time_target - 1692262536) * 100
+    left_id = min(id_1, id_2)
+    right_id = max(id_1, id_2)
+    id_hat = (left_id + right_id) / 2
+
+    result = get_public_match(1, less_than_match_id=id_hat)
+    start_time_hat = result[0]["start_time"]
+    time.sleep(1.5)
+
+    while abs(start_time_hat - time_target) > 60:
+        if start_time_hat > time_target:
+            right_id = id_hat
+            id_hat = round((left_id + right_id) / 2)
+            result = get_public_match(1, less_than_match_id=id_hat)
+            time.sleep(1.5)
+            start_time_hat = result[0]["start_time"]
+
+        else:
+            left_id = id_hat
+            id_hat = round((left_id + right_id) / 2)
+            result = get_public_match(1, less_than_match_id=id_hat)
+            time.sleep(1.5)
+            start_time_hat = result[0]["start_time"]
+
+    result = get_public_match(hundreds_of_match, less_than_match_id=id_hat)
+    result.append(time_target)
+    return [result]
+
+
+def search_hour(filepath, time_start, hours, number_of_match=1):
+    match_sample = []
+    for i in range(0, hours):
+        hour_match = search_id(time_start + (i * 3600),number_of_match)
+        match_sample.append(hour_match)
+        extend_json(filepath, hour_match)
+        print("100 match data saved")
+    return match_sample
